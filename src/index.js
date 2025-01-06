48ba32dd1a43ee04f61414ec3618b5969e5c28a2
heresue <sue.synpark@gmail.com>
2025-01-07 02:25:17 +0900

    Initialize2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <h1 id="title">가위바위보</h1>
+    <h1 id="title">가위 바위 보!</h1>
     <button class="hand">가위</button>
     <button class="hand">바위</button>
     <button class="hand">보</button>
