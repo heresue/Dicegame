da208fe059ab3d50df2ed30e4d6e70a8e1e34a5a
heresue <sue.synpark@gmail.com>
2025-01-07 02:09:25 +0900

    Initialize Dicegame with React

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <h1 id="title">가위바위보</h1>
+    <button class="hand">가위</button>
+    <button class="hand">바위</button>
+    <button class="hand">보</button>
+  </div>
+);
